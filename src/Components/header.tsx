53164344722e346e83ab8790f325eb7e63750879
itallominatti <itallominatti@gmail.com>
2024-07-23 15:43:45 -0300

    [ feat ] main -> Estilização inicial do menu

---
+++ src/Components/header.tsx
@@ -0,0 +1,31 @@
+import { Pizza, Home, UtensilsCrossed } from "lucide-react";
+import { Separator } from "./separator";
+import { Link } from "react-router-dom";
+import { NavLink } from "./nav-link";
+
+
+
+
+
+export function Header() {
+    return (
+        <div className="border-b">
+            <div className="flex h-16 items-center gap-6 px-6">
+                <Pizza className="h-6 w-6" />
+
+                <Separator orientation="vertical" className="h-6" />
+
+                <nav className="flex items-center space-x-4 lg:space-x-6">
+                    <NavLink to='/'>
+                        <Home className="h-4 w-4" />
+                        Início
+                    </NavLink>
+                    <NavLink to='/orders'>
+                        <UtensilsCrossed className="h-4 w-4" />
+                        Pedidos
+                    </NavLink>
+                </nav>
+            </div>
+        </div>
+    )
+}
--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,9 +1,10 @@
+import { Header } from "@/Components/header"
 import { Outlet } from "react-router-dom"
 
 export function AppLayout() {
     return (
         <div className="flex min-h-screen flex-col antialiased">
-            <h1>Cabeçalho</h1>
+            <Header />
 
             <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
                 <Outlet />
